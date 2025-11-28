e89e1f39eb446e79697233d5363619f0fad562d7
Martin Stannard <martin@stannard.id.au>
2025-11-28 20:56:59 +1100

    add auth_helpers

---
+++ test/support/auth_helpers.ex
@@ -0,0 +1,10 @@
+defmodule MedishopWeb.AuthHelpers do
+  import AshAuthentication.Plug.Helpers
+  import Phoenix.ConnTest
+
+  def log_in_user(conn, user) do
+    conn
+    |> init_test_session(%{})
+    |> store_in_session(user)
+  end
+end
